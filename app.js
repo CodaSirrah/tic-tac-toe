b7714323f9640a32ee4ed58af8d29cf0b4c05c94
CodaSirrah <harristurner97@gmail.com>
2021-04-04 18:21:27 +0100

    Added player vs computer option. Improved responsiveness, improved styles and animations, implemented randomised computer moves, added a reset option.

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ displayController() is responsible for displaying the initial grid and displayin
 */
 
 const cells = document.getElementsByClassName("gridCell");
+let endState = false;
 
 const player = (name, type) => {
     return {name, type};
@@ -15,6 +16,7 @@ const player = (name, type) => {
 const player1 = player("Tom", "❌");
 const player2 = player("Ben", "⭕");
 let currentPlayer = player1;
+let selectables = document.getElementsByClassName("select");
 
 const displayController = (() => {
     const displayGameboard = () => {
@@ -23,7 +25,7 @@ const displayController = (() => {
         const gameGrid = document.createElement("div");
         gameGrid.setAttribute("id", "gameGrid");
         main.appendChild(gameGrid);
-        document.querySelector("body").appendChild(main);
+        document.querySelector("#gridSpace").appendChild(main);
         for (i = 1; i <= 9; i++) {
             let cell = document.createElement("div");
             cell.setAttribute("id", `cell${i}`);
@@ -34,53 +36,93 @@ const displayController = (() => {
                 game(cell).gameState();
             });
         };
-        const resetContainer = () => {
-            // document.createElement("div");
-            // const resetText = document.createElement("p");
-            // const resetSymbol = document.createElement("p");
-            // resetText.innerHTML = "Reset"
-            // resetSymbol.innerHTML = "↻";
-            // resetText.classList.add("resetText");
-            // resetSymbol.classList.add("arrow")
-            // resetContainer.classList.add("resetContainer");
-            // resetContainer.appendChild(resetText);
-            // resetContainer.appendChild(resetSymbol);
-            // document.querySelector("body").appendChild(resetContainer);
-        }
-    };
+    }
+    const displayReset = () => {
+        document.createElement("div");
+        const resetContainer = document.createElement("div");
+        const resetText = document.createElement("p");
+        const resetSymbol = document.createElement("p");
+        resetText.innerHTML = "Reset"
+        resetSymbol.innerHTML = "↻";
+        resetText.classList.add("reset");
+        resetSymbol.classList.add("reset")
+        resetContainer.classList.add("resetContainer");
+        resetContainer.classList.add("resetFadeIn");
+        resetContainer.appendChild(resetText);
+        resetContainer.appendChild(resetSymbol);
+        document.querySelector("#tags").appendChild(resetContainer);
+        resetContainer.addEventListener("click", () => {
+            game().resetGrid();
+        })
+    }
+    
     const displayWelcome = () => {
         let titleContainer = document.createElement("div");
-        let titleMessage = document.createElement("h1");
+        let titleTag = document.createElement("h1");
         let startContainer = document.createElement("div");
         let startButton = document.createElement("button");
         let container = document.createElement("div");
         container.classList.add("titleContainer");
         titleContainer.classList.add("titleContainer");
-        titleMessage.classList.add("titleMessage");
+        titleTag.classList.add("titleTag");
         startContainer.classList.add("startContainer");
         startButton.classList.add("startButton");
-        titleMessage.innerHTML = "Tic Tac Toe";
+        titleTag.innerHTML = "Tic Tac Toe";
         startButton.innerHTML = "Start Game?";
-        titleContainer.appendChild(titleMessage);
+        titleContainer.appendChild(titleTag);
         startContainer.appendChild(startButton);
         container.appendChild(titleContainer);
         container.appendChild(startContainer);
         document.querySelector("body").appendChild(container);
         startButton.addEventListener("click", () => {
             startContainer.style.cssText = "animation: fadeOut 0.2s linear forwards;"
-            container.classList.add("startAnimation");
+            container.classList.add("pushUp");
             container.style.cssText = "pointer-events: none";
             displayController.displayGameboard();
+            displayController.displayReset();
+            displayController.displayOpponent();
         });
     };
-    
+
+    const displayOpponent = () => {
+        let opponentContainer = document.createElement("div");
+        let opponentGrid = document.createElement("div");
+        let aiTag = document.createElement("button");
+        let playerTag = document.createElement("button");
+        let vsTag = document.createElement("button");
+        opponentContainer.setAttribute("id", "opponentContainer");
+        opponentGrid.classList.add("opponentGrid");
+        aiTag.classList.add("select");
+        aiTag.classList.add("AI");
+        playerTag.classList.add("select");
+        playerTag.classList.add("human");
+        vsTag.classList.add("vs");  
+        aiTag.innerHTML = "Computer"
+        playerTag.innerHTML = "Player";
+        vsTag.innerHTML = "OR";
+        opponentGrid.appendChild(playerTag);
+        opponentGrid.appendChild(vsTag);
+        opponentGrid.appendChild(aiTag);
+        opponentContainer.appendChild(opponentGrid);
+        document.querySelector("#tags").appendChild(opponentContainer);
+        selectables[1].classList.add("currentSelection");
+        selectables[0].addEventListener("click", () => {
+            selectables[0].classList.add("currentSelection");
+            selectables[1].classList.remove("currentSelection");
+        })
+        selectables[1].addEventListener("click", () => {
+            selectables[1].classList.add("currentSelection");
+            selectables[0].classList.remove("currentSelection");
+        })
+    }
     return {
         displayWelcome,
-        displayGameboard
+        displayGameboard,
+        displayReset,
+        displayOpponent
         };
     })();
 displayController.displayWelcome();
-// displayController.displayGameboard();
 
 const cellArray = document.getElementsByClassName("gridCell");
 
@@ -89,15 +131,15 @@ const game = cell => {
         cell.setAttribute("data-type", type);
     };
     checkEmptyCell = () => {
-        if (cell.dataset.type === "x" || cell.dataset.type === "o") {
+        if (cell.dataset.type === "❌" || cell.dataset.type === "⭕") {
             return false;
         }
         else {
             return true;
         }
     };
-    paintCell = () => {
-        cell.innerHTML = `${currentPlayer.type}`;
+    paintCell = (currentPlayer) => {
+        cell.innerHTML = currentPlayer;
     };
     
     checkResult = (player, playerType) => {
@@ -107,48 +149,68 @@ const game = cell => {
             cellArray[5].dataset.type === playerType || 
             cellArray[6].dataset.type === playerType && cellArray[7].dataset.type === playerType &&
             cellArray[8].dataset.type === playerType) {
-                return victoryScreen(playerType);
+                endState = true;
+                return victoryState(playerType);
         } else if (cellArray[0].dataset.type === playerType && cellArray[3].dataset.type === playerType &&
             cellArray[6].dataset.type === playerType ||
             cellArray[1].dataset.type === playerType && cellArray[4].dataset.type === playerType &&
             cellArray[7].dataset.type === playerType ||
             cellArray[2].dataset.type === playerType && cellArray[5].dataset.type === playerType &&
             cellArray[8].dataset.type === playerType) {
-                return victoryScreen(playerType);
+                endState = true;
+                return victoryState(playerType);
             } else if (cellArray[0].dataset.type === playerType && cellArray[4].dataset.type === playerType &&
                 cellArray[8].dataset.type === playerType ||
                 cellArray[2].dataset.type === playerType && cellArray[4].dataset.type === playerType &&
             cellArray[6].dataset.type === playerType) {
-                return victoryScreen(playerType);
+                endState = true;
+                return victoryState(playerType);
             } else {
                 let gridSpace = 9;
                 for (i = 0; i <= 8; i++) {
                     if (cellArray[i].innerHTML === "❌" || cellArray[i].innerHTML === "⭕")  gridSpace -= 1;
                 }
-                if (gridSpace <= 0) return drawScreen();
-
+                if (gridSpace <= 0)  {
+                    endState = true;
+                    return drawState();
+                }
+                else return false;
             }
     };
 
     gameState = () => {
+        // Player vs Player
+        if (document.getElementsByClassName("currentSelection")[0].childNodes[0].nodeValue === "Player") {
         if (game(cell).checkEmptyCell() === true) {
             if (currentPlayer === player1) {
             game(cell).markCell(player1.type);
-            game(cell).paintCell();
+            game(cell).paintCell(`${currentPlayer.type}`);
             game().checkResult(currentPlayer, currentPlayer.type);
-            
             currentPlayer = player2; 
             } else if (currentPlayer === player2) {
             game(cell).markCell(player2.type);
-            game(cell).paintCell();
+            game(cell).paintCell(`${currentPlayer.type}`);
             game().checkResult(currentPlayer, currentPlayer.type);
-            
             currentPlayer = player1;
             }
         }
+        
+    } 
+    // Player vs Computer
+    else if (document.getElementsByClassName("currentSelection")[0].childNodes[0].nodeValue === "Computer") {
+        currentPlayer = player1;
+        if (game(cell).checkEmptyCell() === true) {
+            game(cell).markCell(player1.type);
+            game(cell).paintCell(`${currentPlayer.type}`);
+            game().checkResult(currentPlayer, currentPlayer.type);
+            if (endState === false) 
+            game().playAI();
+            game().checkResult(player2, player2.type);
+            } 
+        }
     };
 
-    victoryScreen = playerType => {
+    victoryState = playerType => {
         let victoryContainer = document.createElement("div");
         let victoryMessage = document.createElement("p");
         victoryMessage.innerHTML = "The winner is " + playerType;
@@ -157,13 +219,12 @@ const game = cell => {
         victoryContainer.classList.add("victoryContainer");
         gameGrid.appendChild(victoryContainer);
         victoryContainer.addEventListener("click", (e) => {
-            document.getElementById("main").remove();
-            displayController.displayGameboard();
-            
+            game().resetGrid();
+            endState = false;
         })
     };
 
-    drawScreen = () => {
+    drawState = () => {
         let victoryContainer = document.createElement("div");
         let victoryMessage = document.createElement("p");
         victoryMessage.innerHTML = "It's a draw!";
@@ -172,8 +233,8 @@ const game = cell => {
         victoryContainer.classList.add("victoryContainer");
         gameGrid.appendChild(victoryContainer);
         victoryContainer.addEventListener("click", (e) => {
-            document.getElementById("main").remove();
-            displayController.displayGameboard();
+            game().resetGrid();
+            endState = false;
         })
     };
 
@@ -181,6 +242,25 @@ const game = cell => {
         document.getElementById("main").remove();
         displayController.displayGameboard();
     }
+
+    playAI = () => {
+        let newArray = [];
+        for (i = 0; i <= cellArray.length -1; i++ ) {
+            if (game(cellArray[i]).checkEmptyCell() === false) {
+            newArray.push("Full")
+            } else { 
+                newArray.push("Empty");
+            }
+         } 
+        let x = 0;
+        do {
+           x = Math.floor(Math.random() * 9 )
+        } while (newArray[x] !== "Empty"); 
+            game(cellArray[x]).markCell(player2.type);
+            game(cellArray[x]).paintCell(player2.type);
+        
+        
     
-    return {checkEmptyCell, markCell, paintCell, checkResult, gameState, resetGrid};
+    }
+    return {checkEmptyCell, markCell, paintCell, checkResult, gameState, resetGrid, playAI};
 };
